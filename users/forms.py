c501e00e8385a475617d7d5e98613af7e006e985
Vlad Spinul <shinokakaov@mail.ru>
2026-06-17 21:20:46 +0300

    feat: added CustomUser Creation and Change forms

---
+++ users/forms.py
@@ -0,0 +1,25 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
+from users.models import CustomUser
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = CustomUser
+        fields = ['username', 'email', 'password1', 'password2', 'first_name', 'last_name', 'avatar']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].help_text = 'Может содержать буквы, цифры и символы @ . + - _'
+        self.fields['password1'].help_text = 'Пароль должен быть не менее 8 символов, не слишком простым и не состоять только из цифр.'
+        self.fields['password2'].help_text = 'Введите повторно пароль для верификации.'
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = CustomUser
+        fields = ['username', 'first_name', 'last_name', 'avatar']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].help_text = 'Может содержать буквы, цифры и символы @ . + - _'
