fbd83aa96cbb77a2f602e2840bbffa7939239cc4
Vlad Spinul <shinokakaov@mail.ru>
2026-06-29 18:27:33 +0300

    feat: password reset functionality is now available

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
-from django.contrib.auth.views import LoginView, LogoutView
-from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView, PasswordResetDoneView, \
+    PasswordResetConfirmView, PasswordResetCompleteView
+from django.urls import path, reverse_lazy
 
-from core.mixins import StyleFormMixin
 from users.views import *
 
 app_name = "users"
@@ -11,4 +11,24 @@ urlpatterns = [
     path("email-confirm/<str:token>/", email_verification, name="email-confirm"),
     path("login/",LoginView.as_view(template_name="users/login.html"), name="login"),
     path("logout/", LogoutView.as_view(next_page='/'), name="logout"),
+
+    path('password-reset/',
+         PasswordResetView.as_view(
+             template_name='users/password_reset/email_form.html',
+             email_template_name='users/password_reset/email.html',
+             success_url=reverse_lazy('users:password_reset_done')
+         ),
+         name='password_reset'),
+    path('password-reset/done/',
+         PasswordResetDoneView.as_view(template_name='users/password_reset/mail_sent.html'),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',
+         PasswordResetConfirmView.as_view(
+             template_name='users/password_reset/confirm.html',
+             success_url=reverse_lazy('users:password_reset_complete')
+         ),
+         name='password_reset_confirm'),
+    path('reset/done/',
+         PasswordResetCompleteView.as_view(template_name='users/password_reset/complete.html'),
+         name='password_reset_complete'),
 ]
